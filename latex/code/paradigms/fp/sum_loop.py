1a909fa6b0908e339b29ccf0826e7b0312185804
Kilian Lieret <kilian.adriano.lieret@desy.de>
2020-01-17 16:51:26 +0100

    Tiny fixes

--- latex/code/paradigms/fp/sum_loop.py
+++ latex/code/paradigms/fp/sum_loop.py
@@ -1,4 +1,4 @@
-def sum_squres_to(n):
+def sum_squares_to(n):
     result = 0
     for i in range(n+1):
         result += i^2
--- latex/code/paradigms/fp/sum_map_reduce.py
+++ latex/code/paradigms/fp/sum_map_reduce.py
@@ -1,7 +1,7 @@
 from functools import map, reduce
 
 
-def sum_up_to(n):
+def sum_squares_to(n):
     return reduce(
         lambda x, y: x+y, 
         map(lambda x: x**2, range(n+1))
