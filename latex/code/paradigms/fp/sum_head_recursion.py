a7c0bec1c5e91b342d6c01c4985fe33676d29490
Kilian Lieret <kilian.adriano.lieret@desy.de>
2020-01-28 17:08:11 +0100

    Work on suggestions/corrections

--- latex/code/paradigms/fp/sum_head_recursion.py
+++ latex/code/paradigms/fp/sum_head_recursion.py
@@ -1,5 +1,5 @@
-def sum_up_to(n):
+def sum_squares_to(n):
     if n == 0:
         return 0
     else 
-        return n^2 + sum_up_to(n-1)
+        return n^2 + sum_squares_to(n-1)
--- latex/code/paradigms/fp/sum_tail_recursion.py
+++ latex/code/paradigms/fp/sum_tail_recursion.py
@@ -1,4 +1,4 @@
 def sum_squares_to(n, partial_sum=0):
     if n == 0:
         return partial_sum
-    return sum_up_to(n-1, partial_sum + n^2)
+    return sum_squares_to(n-1, partial_sum + n^2)
