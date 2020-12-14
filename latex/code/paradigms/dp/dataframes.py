2302f7f7fe8288a64137d76421c8882ab6b6ed8a
Kilian <kilian.lieret@posteo.de>
2020-12-14 17:48:21 +0100

    Remove these evil tabs

--- latex/code/paradigms/dp/dataframes.py
+++ latex/code/paradigms/dp/dataframes.py
@@ -1,18 +1,18 @@
 # "Chi2 distance" using plain python
 def chi2(data, theory, error):
-	err_sum = 0
-	for i in range(len(data)):
-		if data[i] == theory[i] and error[i] == 0:
-			continue
-		err_sum += (data[i] - theory[i])**2 / (error[i]**2)
-	return err_sum
+    err_sum = 0
+    for i in range(len(data)):
+        if data[i] == theory[i] and error[i] == 0:
+            continue
+        err_sum += (data[i] - theory[i])**2 / (error[i]**2)
+    return err_sum
 
 
 # Using DataFrames: Table contains columns experiment, theory, error
 
 # ROOT RDataFrame example:
 chi2 = ROOT.ROOT.RDataFrame(...)  # initialize
-		.Filter("!(data==theory & error==0.)")  # filter rows
+        .Filter("!(data==theory & error==0.)")  # filter rows
         .Define("sqd", "pow(data-theory, 2) / pow(error, 2)")  # new col
         .Sum("sqd").GetValue()  # sum it up
 
